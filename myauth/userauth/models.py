d1c400b69948470cf9b4f887799ab9216f930886
edunetmastertrainer <mastertrainermern@gmail.com>
2025-12-03 07:32:13 +0530

    Add files via upload
    add

--- myauth/userauth/models.py
+++ myauth/userauth/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.db.models import Model
 # Create your models here.
+class Profile(models.Model):
+    email = models.EmailField(max_length=254)
+    mobile = models.CharField(max_length=15)
+    address = models.TextField()
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    branch = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
--- myauth/userauth/views.py
+++ myauth/userauth/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
-
+from .form import ProfileForm
 
 def register(request):
     if request.method == 'POST':
@@ -15,3 +15,13 @@ def register(request):
 
 def dashboard(request):
     return render(request,'dashboard.html')
+
+def Addprofile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+    else:
+        form = ProfileForm()
+    return render(request, 'profile.html', {'form': form})
